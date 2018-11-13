55d6fe3fc6be04b7ac33b69497860d774d90fd3c
FlowersA32 <44932549+FlowersA32@users.noreply.github.com>
2018-11-13 11:53:58 +0000

    Began Designing calculation tool that stores results and dates through use of streamwriter

--- FlowersA32_120IT_TestCode/FlowersA32_120IT_TestCode/Form1.Designer.cs
+++ FlowersA32_120IT_TestCode/FlowersA32_120IT_TestCode/Form1.Designer.cs
@@ -28,27 +28,265 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblInformation = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mtbIn1 = new System.Windows.Forms.MaskedTextBox();
+            this.mtbIn3 = new System.Windows.Forms.MaskedTextBox();
+            this.mtbIn5 = new System.Windows.Forms.MaskedTextBox();
+            this.mtbIn7 = new System.Windows.Forms.MaskedTextBox();
+            this.mtbIn2 = new System.Windows.Forms.MaskedTextBox();
+            this.mtbIn4 = new System.Windows.Forms.MaskedTextBox();
+            this.mtbIn6 = new System.Windows.Forms.MaskedTextBox();
+            this.mtbIn8 = new System.Windows.Forms.MaskedTextBox();
+            this.cbIn1 = new System.Windows.Forms.ComboBox();
+            this.cbIn4 = new System.Windows.Forms.ComboBox();
+            this.cbIn3 = new System.Windows.Forms.ComboBox();
+            this.cbIn2 = new System.Windows.Forms.ComboBox();
+            this.lblEq1 = new System.Windows.Forms.Label();
+            this.lblEq2 = new System.Windows.Forms.Label();
+            this.lblEq3 = new System.Windows.Forms.Label();
+            this.lblEq4 = new System.Windows.Forms.Label();
+            this.lblOut1 = new System.Windows.Forms.Label();
+            this.lblOut2 = new System.Windows.Forms.Label();
+            this.lblOut3 = new System.Windows.Forms.Label();
+            this.lblOut4 = new System.Windows.Forms.Label();
+            this.dtPicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // lblInformation
+            // mtbIn1
             // 
-            this.lblInformation.AutoSize = true;
-            this.lblInformation.Font = new System.Drawing.Font("Adobe Gothic Std B", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblInformation.Location = new System.Drawing.Point(444, 126);
-            this.lblInformation.Name = "lblInformation";
-            this.lblInformation.Size = new System.Drawing.Size(212, 48);
-            this.lblInformation.TabIndex = 0;
-            this.lblInformation.Text = "Click one of the buttons below to \r\nbe taken to the task you want to\r\nuse";
+            this.mtbIn1.Location = new System.Drawing.Point(143, 179);
+            this.mtbIn1.Name = "mtbIn1";
+            this.mtbIn1.Size = new System.Drawing.Size(100, 20);
+            this.mtbIn1.TabIndex = 0;
+            // 
+            // mtbIn3
+            // 
+            this.mtbIn3.Location = new System.Drawing.Point(143, 208);
+            this.mtbIn3.Name = "mtbIn3";
+            this.mtbIn3.Size = new System.Drawing.Size(100, 20);
+            this.mtbIn3.TabIndex = 1;
+            // 
+            // mtbIn5
+            // 
+            this.mtbIn5.Location = new System.Drawing.Point(143, 234);
+            this.mtbIn5.Name = "mtbIn5";
+            this.mtbIn5.Size = new System.Drawing.Size(100, 20);
+            this.mtbIn5.TabIndex = 2;
+            // 
+            // mtbIn7
+            // 
+            this.mtbIn7.Location = new System.Drawing.Point(143, 260);
+            this.mtbIn7.Name = "mtbIn7";
+            this.mtbIn7.Size = new System.Drawing.Size(100, 20);
+            this.mtbIn7.TabIndex = 3;
+            // 
+            // mtbIn2
+            // 
+            this.mtbIn2.Location = new System.Drawing.Point(290, 179);
+            this.mtbIn2.Name = "mtbIn2";
+            this.mtbIn2.Size = new System.Drawing.Size(100, 20);
+            this.mtbIn2.TabIndex = 4;
+            // 
+            // mtbIn4
+            // 
+            this.mtbIn4.Location = new System.Drawing.Point(290, 208);
+            this.mtbIn4.Name = "mtbIn4";
+            this.mtbIn4.Size = new System.Drawing.Size(100, 20);
+            this.mtbIn4.TabIndex = 5;
+            // 
+            // mtbIn6
+            // 
+            this.mtbIn6.Location = new System.Drawing.Point(290, 234);
+            this.mtbIn6.Name = "mtbIn6";
+            this.mtbIn6.Size = new System.Drawing.Size(100, 20);
+            this.mtbIn6.TabIndex = 6;
+            // 
+            // mtbIn8
+            // 
+            this.mtbIn8.Location = new System.Drawing.Point(290, 260);
+            this.mtbIn8.Name = "mtbIn8";
+            this.mtbIn8.Size = new System.Drawing.Size(100, 20);
+            this.mtbIn8.TabIndex = 7;
+            // 
+            // cbIn1
+            // 
+            this.cbIn1.FormattingEnabled = true;
+            this.cbIn1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "/",
+            "*"});
+            this.cbIn1.Location = new System.Drawing.Point(249, 178);
+            this.cbIn1.Name = "cbIn1";
+            this.cbIn1.Size = new System.Drawing.Size(35, 21);
+            this.cbIn1.TabIndex = 8;
+            // 
+            // cbIn4
+            // 
+            this.cbIn4.FormattingEnabled = true;
+            this.cbIn4.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "/",
+            "*"});
+            this.cbIn4.Location = new System.Drawing.Point(249, 259);
+            this.cbIn4.Name = "cbIn4";
+            this.cbIn4.Size = new System.Drawing.Size(35, 21);
+            this.cbIn4.TabIndex = 9;
+            // 
+            // cbIn3
+            // 
+            this.cbIn3.FormattingEnabled = true;
+            this.cbIn3.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "/",
+            "*"});
+            this.cbIn3.Location = new System.Drawing.Point(249, 232);
+            this.cbIn3.Name = "cbIn3";
+            this.cbIn3.Size = new System.Drawing.Size(35, 21);
+            this.cbIn3.TabIndex = 10;
+            // 
+            // cbIn2
+            // 
+            this.cbIn2.FormattingEnabled = true;
+            this.cbIn2.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "/",
+            "*"});
+            this.cbIn2.Location = new System.Drawing.Point(249, 205);
+            this.cbIn2.Name = "cbIn2";
+            this.cbIn2.Size = new System.Drawing.Size(35, 21);
+            this.cbIn2.TabIndex = 11;
+            // 
+            // lblEq1
+            // 
+            this.lblEq1.AutoSize = true;
+            this.lblEq1.Location = new System.Drawing.Point(396, 181);
+            this.lblEq1.Name = "lblEq1";
+            this.lblEq1.Size = new System.Drawing.Size(13, 13);
+            this.lblEq1.TabIndex = 12;
+            this.lblEq1.Text = "=";
+            // 
+            // lblEq2
+            // 
+            this.lblEq2.AutoSize = true;
+            this.lblEq2.Location = new System.Drawing.Point(396, 211);
+            this.lblEq2.Name = "lblEq2";
+            this.lblEq2.Size = new System.Drawing.Size(13, 13);
+            this.lblEq2.TabIndex = 13;
+            this.lblEq2.Text = "=";
+            // 
+            // lblEq3
+            // 
+            this.lblEq3.AutoSize = true;
+            this.lblEq3.Location = new System.Drawing.Point(396, 237);
+            this.lblEq3.Name = "lblEq3";
+            this.lblEq3.Size = new System.Drawing.Size(13, 13);
+            this.lblEq3.TabIndex = 14;
+            this.lblEq3.Text = "=";
+            // 
+            // lblEq4
+            // 
+            this.lblEq4.AutoSize = true;
+            this.lblEq4.Location = new System.Drawing.Point(396, 262);
+            this.lblEq4.Name = "lblEq4";
+            this.lblEq4.Size = new System.Drawing.Size(13, 13);
+            this.lblEq4.TabIndex = 15;
+            this.lblEq4.Text = "=";
+            // 
+            // lblOut1
+            // 
+            this.lblOut1.AutoSize = true;
+            this.lblOut1.Location = new System.Drawing.Point(415, 182);
+            this.lblOut1.Name = "lblOut1";
+            this.lblOut1.Size = new System.Drawing.Size(0, 13);
+            this.lblOut1.TabIndex = 16;
+            // 
+            // lblOut2
+            // 
+            this.lblOut2.AutoSize = true;
+            this.lblOut2.Location = new System.Drawing.Point(415, 211);
+            this.lblOut2.Name = "lblOut2";
+            this.lblOut2.Size = new System.Drawing.Size(0, 13);
+            this.lblOut2.TabIndex = 17;
+            // 
+            // lblOut3
+            // 
+            this.lblOut3.AutoSize = true;
+            this.lblOut3.Location = new System.Drawing.Point(415, 237);
+            this.lblOut3.Name = "lblOut3";
+            this.lblOut3.Size = new System.Drawing.Size(0, 13);
+            this.lblOut3.TabIndex = 18;
+            // 
+            // lblOut4
+            // 
+            this.lblOut4.AutoSize = true;
+            this.lblOut4.Location = new System.Drawing.Point(415, 263);
+            this.lblOut4.Name = "lblOut4";
+            this.lblOut4.Size = new System.Drawing.Size(0, 13);
+            this.lblOut4.TabIndex = 19;
+            // 
+            // dtPicker1
+            // 
+            this.dtPicker1.Location = new System.Drawing.Point(12, 574);
+            this.dtPicker1.Name = "dtPicker1";
+            this.dtPicker1.Size = new System.Drawing.Size(200, 20);
+            this.dtPicker1.TabIndex = 20;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(143, 345);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 21;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(143, 374);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 22;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1124, 606);
-            this.Controls.Add(this.lblInformation);
+            this.ClientSize = new System.Drawing.Size(773, 606);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.dtPicker1);
+            this.Controls.Add(this.lblOut4);
+            this.Controls.Add(this.lblOut3);
+            this.Controls.Add(this.lblOut2);
+            this.Controls.Add(this.lblOut1);
+            this.Controls.Add(this.lblEq4);
+            this.Controls.Add(this.lblEq3);
+            this.Controls.Add(this.lblEq2);
+            this.Controls.Add(this.lblEq1);
+            this.Controls.Add(this.cbIn2);
+            this.Controls.Add(this.cbIn3);
+            this.Controls.Add(this.cbIn4);
+            this.Controls.Add(this.cbIn1);
+            this.Controls.Add(this.mtbIn8);
+            this.Controls.Add(this.mtbIn6);
+            this.Controls.Add(this.mtbIn4);
+            this.Controls.Add(this.mtbIn2);
+            this.Controls.Add(this.mtbIn7);
+            this.Controls.Add(this.mtbIn5);
+            this.Controls.Add(this.mtbIn3);
+            this.Controls.Add(this.mtbIn1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SuperHappyFunTime";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,7 +294,29 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblInformation;
+        private System.Windows.Forms.MaskedTextBox mtbIn1;
+        private System.Windows.Forms.MaskedTextBox mtbIn3;
+        private System.Windows.Forms.MaskedTextBox mtbIn5;
+        private System.Windows.Forms.MaskedTextBox mtbIn7;
+        private System.Windows.Forms.MaskedTextBox mtbIn2;
+        private System.Windows.Forms.MaskedTextBox mtbIn4;
+        private System.Windows.Forms.MaskedTextBox mtbIn6;
+        private System.Windows.Forms.MaskedTextBox mtbIn8;
+        private System.Windows.Forms.ComboBox cbIn1;
+        private System.Windows.Forms.ComboBox cbIn4;
+        private System.Windows.Forms.ComboBox cbIn3;
+        private System.Windows.Forms.ComboBox cbIn2;
+        private System.Windows.Forms.Label lblEq1;
+        private System.Windows.Forms.Label lblEq2;
+        private System.Windows.Forms.Label lblEq3;
+        private System.Windows.Forms.Label lblEq4;
+        private System.Windows.Forms.Label lblOut1;
+        private System.Windows.Forms.Label lblOut2;
+        private System.Windows.Forms.Label lblOut3;
+        private System.Windows.Forms.Label lblOut4;
+        private System.Windows.Forms.DateTimePicker dtPicker1;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnReset;
     }
 }
 
--- FlowersA32_120IT_TestCode/FlowersA32_120IT_TestCode/Form1.cs
+++ FlowersA32_120IT_TestCode/FlowersA32_120IT_TestCode/Form1.cs
@@ -15,6 +15,112 @@ namespace FlowersA32_120IT_TestCode
         public Form1()
         {
             InitializeComponent();
+            cbIn1.SelectedIndex = 0;
+            cbIn2.SelectedIndex = 0;
+            cbIn3.SelectedIndex = 0;
+            cbIn4.SelectedIndex = 0;
+            mtbIn1.KeyPress += MtbIn1_KeyPress;
+            mtbIn2.KeyPress += MtbIn2_KeyPress;
+        }
+
+        private void MtbIn2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            const char delLett = (char)8;
+            e.Handled = !char.IsDigit(e.KeyChar) && e.KeyChar != delLett;
+        }
+
+        private void MtbIn1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            const char delLett = (char)8;
+            e.Handled = !char.IsDigit(e.KeyChar) && e.KeyChar != delLett;
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+
+            if (cbIn1.Text == "+")
+            {
+                decimal res1 = Convert.ToDecimal(mtbIn1.Text) + Convert.ToDecimal(mtbIn2.Text);
+                lblOut1.Text = res1.ToString();
+            }
+            else if (cbIn1.Text == "-")
+            {
+                decimal res1 = Convert.ToDecimal(mtbIn1.Text) - Convert.ToDecimal(mtbIn2.Text);
+                lblOut1.Text = res1.ToString();
+            }
+            else if (cbIn1.Text == "/")
+            {
+                decimal res1 = Convert.ToDecimal(mtbIn1.Text) / Convert.ToDecimal(mtbIn2.Text);
+                lblOut1.Text = res1.ToString();
+            }
+            else if (cbIn1.Text == "*")
+            {
+                decimal res1 = Convert.ToDecimal(mtbIn1.Text) * Convert.ToDecimal(mtbIn2.Text);
+                lblOut1.Text = res1.ToString();
+            }
+
+            if (cbIn2.Text == "+")
+            {
+                decimal res2 = Convert.ToDecimal(mtbIn3.Text) + Convert.ToDecimal(mtbIn4.Text);
+                lblOut2.Text = res2.ToString();
+            }
+            else if (cbIn2.Text == "-")
+            {
+                decimal res2 = Convert.ToDecimal(mtbIn3.Text) - Convert.ToDecimal(mtbIn4.Text);
+                lblOut2.Text = res2.ToString();
+            }
+            else if (cbIn2.Text == "/")
+            {
+                decimal res2 = Convert.ToDecimal(mtbIn3.Text) / Convert.ToDecimal(mtbIn4.Text);
+                lblOut2.Text = res2.ToString();
+            }
+            else if (cbIn2.Text == "*")
+            {
+                decimal res2 = Convert.ToDecimal(mtbIn3.Text) * Convert.ToDecimal(mtbIn4.Text);
+                lblOut2.Text = res2.ToString();
+            }
+
+            if (cbIn3.Text == "+")
+            {
+                decimal res3 = Convert.ToDecimal(mtbIn5.Text) + Convert.ToDecimal(mtbIn6.Text);
+                lblOut3.Text = res3.ToString();
+            }
+            else if (cbIn3.Text == "-")
+            {
+                decimal res3 = Convert.ToDecimal(mtbIn5.Text) - Convert.ToDecimal(mtbIn6.Text);
+                lblOut3.Text = res3.ToString();
+            }
+            else if (cbIn3.Text == "/")
+            {
+                decimal res3 = Convert.ToDecimal(mtbIn5.Text) / Convert.ToDecimal(mtbIn6.Text);
+                lblOut3.Text = res3.ToString();
+            }
+            else if (cbIn3.Text == "*")
+            {
+                decimal res3 = Convert.ToDecimal(mtbIn5.Text) * Convert.ToDecimal(mtbIn6.Text);
+                lblOut3.Text = res3.ToString();
+            }
+
+            if (cbIn4.Text == "+")
+            {
+                decimal res4 = Convert.ToDecimal(mtbIn7.Text) + Convert.ToDecimal(mtbIn8.Text);
+                lblOut4.Text = res4.ToString();
+            }
+            else if (cbIn4.Text == "-")
+            {
+                decimal res4 = Convert.ToDecimal(mtbIn7.Text) - Convert.ToDecimal(mtbIn8.Text);
+                lblOut4.Text = res4.ToString();
+            }
+            else if (cbIn4.Text == "/")
+            {
+                decimal res4 = Convert.ToDecimal(mtbIn7.Text) / Convert.ToDecimal(mtbIn8.Text);
+                lblOut4.Text = res4.ToString();
+            }
+            else if (cbIn4.Text == "*")
+            {
+                decimal res4 = Convert.ToDecimal(mtbIn7.Text) * Convert.ToDecimal(mtbIn8.Text);
+                lblOut4.Text = res4.ToString();
+            }
         }
     }
 }
